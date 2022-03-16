172a38bbfdf02c2b0ad85432f58629d3b40757e7
Cody Kazuma <kodalpha@Codys-MacBook-Pro.local>
2022-03-16 16:56:43 -0700

    First Commit

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,29 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  UserID INT NOT NULL AUTO_INCREMENT,
+  Username VARCHAR(20) NOT NULL,
+  PRIMARY KEY (UserID)
 );
+
+CREATE TABLE rooms (
+  RoomID INT NOT NULL AUTO_INCREMENT,
+  Roomname VARCHAR(20) NOT NULL,
+  PRIMARY KEY (RoomID)
+);
+
+CREATE TABLE messages (
+  MessageID INT NOT NULL AUTO_INCREMENT,
+  MessageText VARCHAR(300) NOT NULL,
+  ID_Users INT NOT NULL,
+  ID_Roomname INT NOT NULL,
+  PRIMARY KEY (MessageID),
+  FOREIGN KEY (ID_Users) REFERENCES users(UserID),
+  FOREIGN KEY (ID_Roomname) REFERENCES rooms(RoomID)
+);
+
+  /* Describe your table here.*/
 
 /* Create other tables and define schemas for them here! */
 
